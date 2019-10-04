bb1b4b3c2c4993d5c07afe2ab2eb1b150de84c4f
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2019-10-04 16:58:56 +0800

    Add webpack aliases and configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,37 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const path = require("path");
+const webpack = require("webpack");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
+const config = {
+    module: {
+        rules:[
+            {
+                test:'/\.vue$/',
+                loader:'vue-loader',
+            }
+        ]
+
+    },
+    plugins: [
+        new VueLoaderPlugin()
+        //new webpack.optimize.UglifyJsPlugin({/* options here */}),
+        //new webpack.optimize.ModuleConcatenationPlugin(),
+    ],
+    output: {
+        filename: "[name].js",
+        chunkFilename: "js/[id]-[hash].js",
+        publicPath: "/"
+    },
+    resolve: {
+        alias: {
+            "@src": path.resolve(__dirname, "resources/js"),
+            "@views": path.resolve(__dirname, "resources/js", "views"),
+            "@layout": path.resolve(__dirname, "resources/js", "layout"),
+            "@api": path.resolve(__dirname, "resources/js", "api"),
+        }
+    }
+};
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -10,6 +42,28 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+const vendors = [
+    "vue",
+    "vue-router",
+    "vee-validate",
+    "vuex",
+    "vuex-persistedstate",
+    "vue-bus",
+    "vue-svgicon",
+    "moment",
+    "filesize",
+    "cleave.js",
+    "axios",
+    "./resources/js/core/index.js",
+    "./resources/js/core/api-builder/index.js",
+    "./resources/js/core/store-builder/index.js"
+];
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.webpackConfig(config)
+    .extract(vendors, "public/js/vendor.js")
+    .js("resources/js/app.js", "public/js/app.js")
+    .sass("resources/sass/app.scss", "public/css/app.css");
+
+if (mix.inProduction()) {
+    mix.version();
+}
