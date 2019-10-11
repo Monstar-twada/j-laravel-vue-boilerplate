a8c60d28bec35a986cc69b0fbdeeb912d07c9cc0
Allen Jay Desamparado <ajDesamparado13@gmail.com>
2019-10-11 15:47:20 +0800

    Enclose api-routes with api-middleware

--- routes/api.php
+++ routes/api.php
@@ -12,27 +12,28 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group(['middleware' => 'api'],function(){
+    Route::middleware('auth:api')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+    Route::group(['prefix' => 'users'], function () {
+        $c = 'Auth\LoginController';
+        Route::post('authenticate', $c . '@authenticateAny');
+        //Route::post('refresh', $c.'@refresh');
+        Route::post('forgot-password', 'Auth\ForgotPasswordController@sendResetLink');
+        Route::post('change-password', 'Auth\ResetPasswordController@resetPass');
+        Route::post('register', 'Api\UserController@store');
+    });
 
-Route::group(['prefix' => 'users'], function () {
-    $c = 'Auth\LoginController';
-    Route::post('authenticate', $c . '@authenticateAny');
-    //Route::post('refresh', $c.'@refresh');
-    Route::post('forgot-password', 'Auth\ForgotPasswordController@sendResetLink');
-    Route::post('change-password', 'Auth\ResetPasswordController@resetPass');
-    Route::post('register', 'Api\UserController@store');
-});
-
-Route::group(['prefix' => 'users'], function () {
-    $c = 'Api\UserController';
-    Route::post('', $c . '@store');
-    Route::get('', $c . '@index');
-    Route::get('all', $c . '@all');
-    Route::get('{id}', $c . '@show');
-    Route::delete('{id}', $c . '@destroy');
-    Route::put('{id}', $c . '@update');
-});
-Route::group(['middleware' => 'jwt.auth'], function () { });
+    Route::group(['prefix' => 'users'], function () {
+        $c = 'Api\UserController';
+        Route::post('', $c . '@store');
+        Route::get('', $c . '@index');
+        Route::get('all', $c . '@all');
+        Route::get('{id}', $c . '@show');
+        Route::delete('{id}', $c . '@destroy');
+        Route::put('{id}', $c . '@update');
+    });
+    Route::group(['middleware' => 'jwt.auth'], function () { });
+})
